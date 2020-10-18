9f8429e4888221d37aca9f3056426f4c9773184e
Thijs <thijsvb@gmail.com>
2020-10-18 17:02:46 +0200

    added a lot of comments

--- classes.py
+++ classes.py
@@ -1,34 +1,45 @@
 import pygame
 from pygame.locals import *
+import math
 
 
 class Ball:
     def __init__(self, surface, x, y, r = 10, color = (255, 255, 255)):
+        # Keep track of the position and velocity in vectors, this makes the physics easier later on
         self.pos = pygame.math.Vector2(x, y)
         self.vel = pygame.math.Vector2(0, 0)
         self.r = r
         self.color = color
-        self.screen = surface
-        self.friction = 0.01
 
-    def move(self):
-        if self.vel.magnitude() < 0.1:
-            self.vel.xy = (0, 0)
-        else:
-            fricVec = self.vel.normalize() * - self.friction
-            self.vel += fricVec
-            self.pos += self.vel
+        self.screen = surface # pygame wants to know where to draw things, so I added the screen as an argument
+
+        self.friction = 0.01 # this is just a constant to make the ball slow down over time, I put it here so it's easy to tweak if needed
 
     def draw(self):
+        # The position x and y are not whole numbers (and pygame wants exact pixel locations), so cast them as ints
         x = int(self.pos.x)
         y = int(self.pos.y)
         pygame.draw.circle(self.screen, self.color, (x,y), self.r)
 
+    def move(self):
+        # If the velocity of the ball is super low, just kill it, otherwise it keeps going veeery slowly for forever
+        if self.vel.magnitude() < 0.1:
+            self.vel.xy = (0, 0)
+        # If the velocity is high enough, move the ball
+        else:
+            self.vel *= (1-self.friction) # First decrease the velocity a tiny bit to model friction
+            self.pos += self.vel # Then add the velocity vector to the position.
+            # A bit more on the physics of this: normally the change in position dx = v * dt, but since our timestep dt is always the same here (the time between two frames). We can just do all our physics leaving that factor out.
+            # Because position and velocity are both vector, the addition works for both x and y
+
     def accelerate(self, a):
+        # Similair to the move function, for acceleration you add the acceleration vector to the velocity vector
         self.vel += a
+        # If the ball goes to fast, it'll end up flying through walls and things like that
+        # So here I limit the velocity so the ball will not move more then it's own radius in one frame
         if self.vel.magnitude() > self.r:
             self.vel.scale_to_length(self.r)
-        
+
     def bounce(self, walls):
         for wall in walls:
             x, y = self.pos.xy
@@ -72,22 +83,24 @@ class Wall:
 
     def draw(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
-        
+
 class Goal:
     def __init__(self, surface, x, y, r = 10, color = (0,0,0)):
         self.pos = pygame.math.Vector2(x,y)
         self.r = r
         self.color = color
         self.screen = surface
-    
+
     def draw(self):
         x = int(self.pos.x)
         y = int(self.pos.y)
         pygame.draw.circle(self.screen, self.color, (x,y), self.r)
-        
+
     def hit(self,ball):
-        #has to return True if the ball is touching the target
-        #Roy: But now, I tried to do it in an if-statement in the game code
-        #Roy: However, this doesn't work very well
-        return
-        
+        # Thijs: your code correct, it was just in the wrong spot with the event handling, so I moved it over here and added returns.
+        distance = math.hypot(ball.pos.x-self.pos.x,ball.pos.y-self.pos.y)#Roy: I added this line so it updates the distance everytime we move the ball
+        if distance <= (ball.r+self.r): #Roy: This is the if-statement that checks for collision, e.g. if the ball hits the target
+            self.color =  (0,0,255) #Roy: for now, I just told the code to change the color of the goal, we have to see what we want the program to do once the goal is hit
+            return True
+        else:
+            return False
--- game.py
+++ game.py
@@ -6,12 +6,14 @@ from classes import Ball, Goal, Wall
 # Initialise screen
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Test')
-framerate = 60
+pygame.display.set_caption('Game name here') # maybe we should come up with a name
+framerate = 60 # I set this framerate to use later
 screenWidth, screenHeight = screen.get_size()
+
+# Setting up game objects
 b = Ball(screen, screenWidth/2, 500) #Roy: I changed the location of the ball to the middle of the bottom of the screen
 g = Goal(screen, 400, 100) #Roy: And I added the goal on the top of the screen, also in the middle
-distance = math.hypot(b.pos.x-g.pos.x,b.pos.y-g.pos.y) #Roy: This line calculates the distance between the ball and the goal
+# distance = math.hypot(b.pos.x-g.pos.x,b.pos.y-g.pos.y) #Roy: This line calculates the distance between the ball and the goal. Thijs: I commented this line out, cause I don't beleive the variable is used here
 walls = [Wall(screen, 0, 0, "horizontal", screenWidth),
          Wall(screen, 0, 0, "vertical", screenHeight),
          Wall(screen, screenWidth-10, 0, "vertical", screenHeight),
@@ -24,26 +26,32 @@ loop = True
 while loop:
     # This for loop checks the pygame events: things like user inputs get turned into those events
     for event in pygame.event.get():
+        # This event handles close the window
         if event.type == QUIT:
             loop = False
+        # This event handles when the user quits
         if event.type == MOUSEBUTTONDOWN:
             mousePos = pygame.math.Vector2(event.pos)
             acc = (mousePos - b.pos).normalize()
             acc *= 3
             b.accelerate(acc)
-            distance = math.hypot(b.pos.x-g.pos.x,b.pos.y-g.pos.y)#Roy: I added this line so it updates the distance everytime we move the ball
-        if distance <= (b.r+g.r): #Roy: This is the if-statement that checks for collision, e.g. if the ball hits the target
-            g.color =  (0,0,255) #Roy: for now, I just told the code to change the color of the goal, we have to see what we want the program to do once the goal is hit
 
+    screen.fill((0, 127, 50)) # set a background on top of everything that was drawn last frame
 
-    screen.fill((0, 127, 50))
+    # Update the physics
     b.bounce(walls)
     b.move()
-    b.draw()
+    g.hit(b)
+
+    # Draw all the objects
     g.draw()
     for wall in walls:
         wall.draw()
-    pygame.display.update()
-    pygame.time.delay(int(1000/framerate))
+    b.draw()
 
+    # Update the screen and wait for the next frame (without the delay the game runs way too fast)
+    pygame.display.update()
+    pygame.time.delay(int(1000/framerate)) # The delay function takes a time in milliseconds, the framerate is in frames per second so the delay time t = 1000/ framerate
+
+# If we leave the game loop; quit
 pygame.quit()
