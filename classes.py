05a3a6c2e7f4e09ab88931b6a0a8c3f87ac7acb3
Thijs <thijsvb@gmail.com>
2020-10-20 14:56:57 +0200

    now everything is with a capital :)

--- classes.py
+++ classes.py
@@ -161,7 +161,7 @@ class scoreSheet():
                     self.screen.blit(blit, (startx + 170, starty + 10 + ((i - 1) * (self.height/(Nlevels+1)))))
             pygame.draw.line(self.screen, (0,0,0), (startx, starty + (i * (self.height/(Nlevels+1)))), (startx + self.width, starty + (i * (self.height / (Nlevels+1)))), 2)
         pygame.display.update()
-        pygame.time.delay(10000)
+        pygame.time.delay(60000)
         # event = pygame.event.get()[0]
         # while event.type != MOUSEBUTTONDOWN:
         #     event = pygame.event.get()
