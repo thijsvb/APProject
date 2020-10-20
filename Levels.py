765e1057dc3c6697f1b40e90217d44d17087ddc9
kaidllc <72605962+kaidllc@users.noreply.github.com>
2020-10-20 08:36:01 +0200

    Update Levels.py

--- Levels.py
+++ Levels.py
@@ -1,3 +1,7 @@
+import pygame
+from pygame.locals import *
+from classes import Ball, Goal, Wall, scoreSheet
+
 class Level:
     #Generic super-class used to define a level. For each level with level" 
     #specific info we should create a separate child class"
@@ -5,18 +9,24 @@ class Level:
     def __init__(self, ball):
         
         # Lists of sprites used in all levels.
-        self.wall_list = None
-        
         self.background = None   #In parent class none; we can change this per level
-        self.wall_list = pygame.sprite.Group()  #list of walls; see child class
         self.ball = ball
-
-    def update(self):
-        self.wall_list.update() #Update everything in this level
+#        self.goal = goal
         
-    def draw(self, screen):
-        #Draw everything on this level
-        self.wall_list.draw(screen)
+        screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption('Game name here') # maybe we should come up with a name
+        framerate = 60 # I set this framerate to use later
+        screenWidth, screenHeight = screen.get_size()
+ 
+        b = Ball(screen, screenWidth/2, 500)
+        g = Goal(screen, 400, 100)
+
+#    def update(self):
+#        self.wall_list.update() #Update everything in this level
+#        
+#    def draw(self, screen):
+#        #Draw everything on this level
+#        self.wall_list.draw(screen)
 
 
 
@@ -25,26 +35,29 @@ class Level_01(Level):
     
     def __init__(self, ball):
         Level.__init__(self, ball) # Call the parent constructor
-        self.background = pygame.image.load("PUT BACKGROUND IMAGE HERE").convert()
-        self.background.set_colorkey(PUT COLOR HERE)
+#        self.background = pygame.image.load("PUT BACKGROUND IMAGE HERE").convert()
+#        self.background.set_colorkey(PUT COLOR HERE)
         
-
+        screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption('Game name here') # maybe we should come up with a name
+        framerate = 60 # I set this framerate to use later
+        screenWidth, screenHeight = screen.get_size()
+ 
+        b = Ball(screen, screenWidth/2, 500)
+        g = Goal(screen, 400, 100)
+        
         walls = [Wall(screen, 0, 0, "horizontal", screenWidth),
                  Wall(screen, 0, 0, "vertical", screenHeight),
                  Wall(screen, screenWidth-10, 0, "vertical", screenHeight),
                  Wall(screen, 0, screenHeight-10, "horizontal", screenWidth),
                  Wall(screen, 200, 200, "horizontal", 200),
                  Wall(screen, 500, 200, "vertical", 300)] 
-
-        
-        # Go through the array above and add platforms
+       
+        b.draw()
+        g.draw()
         for wall in walls:
-            #example below
-            block = platforms.Platform(platform[0])
-            block.rect.x = platform[1]
-            block.rect.y = platform[2]
-            block.player = self.player
-            self.platform_list.add(block)
+            wall.draw()
+            b.draw()
             
             
             
@@ -53,22 +66,25 @@ class Level_02(Level):
     
     def __init__(self, ball):
         Level.__init__(self, ball) # Call the parent constructor
-        self.background = pygame.image.load("PUT BACKGROUND IMAGE HERE").convert()
-        self.background.set_colorkey(PUT COLOR HERE)
+#        self.background = pygame.image.load("PUT BACKGROUND IMAGE HERE").convert()
+#        self.background.set_colorkey(PUT COLOR HERE)
         
-
+        screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption('Game name here') # maybe we should come up with a name
+        framerate = 60 # I set this framerate to use later
+        screenWidth, screenHeight = screen.get_size()
+ 
+        b = Ball(screen, screenWidth/2, 500)
+        g = Goal(screen, 400, 100)
+        
         walls = [Wall(screen, 0, 0, "horizontal", screenWidth),
                  Wall(screen, 0, 0, "vertical", screenHeight),
                  Wall(screen, screenWidth-10, 0, "vertical", screenHeight),
                  Wall(screen, 0, screenHeight-10, "horizontal", screenWidth)]
                 #Only has surrounding walls now
-
         
-        # Go through the array above and add platforms
+        b.draw()
+        g.draw()
         for wall in walls:
-            #example below
-            block = platforms.Platform(platform[0])
-            block.rect.x = platform[1]
-            block.rect.y = platform[2]
-            block.player = self.player
-            self.platform_list.add(block)
+            wall.draw()
+            b.draw()
