200290061b74e9d1a9afd8d30cc5ee4265461d98
kaidllc <72605962+kaidllc@users.noreply.github.com>
2020-10-20 13:14:43 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -42,29 +42,29 @@ def main():
                     b.accelerate(acc)
                     strokes += 1
 
-    screen.fill((0, 127, 50)) # set a background on top of everything that was drawn last frame
+        screen.fill((0, 127, 50)) # set a background on top of everything that was drawn last frame
 
-    # Update the physics
-    b.bounce(walls)
-    b.move()
-    g.hit(b)
+        # Update the physics
+        b.bounce(walls)
+        b.move()
+        g.hit(b)
 
-    # Draw all the objects
-    g.draw()
-    for wall in walls:
-        wall.draw()
-    b.draw()
-    if g.hit(b):
-         sheet.drawSheet(strokes)
-         current_level_no += 1
-         current_level = level_list[current_level_no]
-         b.level = current_level
+        # Draw all the objects
+        g.draw()
+        for wall in walls:
+            wall.draw()
+        b.draw()
+        if g.hit(b):
+            sheet.drawSheet(strokes)
+            current_level_no += 1
+            current_level = level_list[current_level_no]
+            b.level = current_level
     
-    current_level.draw(screen)
+        current_level.draw(screen)
    
-    # Update the screen and wait for the next frame (without the delay the game runs way too fast)
-    pygame.display.update()
-    pygame.time.delay(int(1000/framerate)) # The delay function takes a time in milliseconds, the framerate is in frames per second so the delay time t = 1000/ framerate
+        # Update the screen and wait for the next frame (without the delay the game runs way too fast)
+        pygame.display.update()
+        pygame.time.delay(int(1000/framerate)) # The delay function takes a time in milliseconds, the framerate is in frames per second so the delay time t = 1000/ framerate
     
     # If we leave the game loop; quit
     pygame.quit()
